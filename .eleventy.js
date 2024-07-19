33955b9fb3e5a33bd95ceeafb6d93f4a14015ab5
ivalynx <inkybasik@gmail.com>
2024-07-19 16:21:32 +0300

    install eleventy-navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 // 11ty config documentation https://www.11ty.dev/docs/config/
 
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 	return {
 		dir: {
 			output: "dist",
