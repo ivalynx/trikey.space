ef5e190cfe40352aa15b5148c65941c559522203
ivalynx <inkybasik@gmail.com>
2025-02-22 09:32:49 +0200

    added script Image to eleventy.js but it don't work and you can see it

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,24 @@
 // 11ty config documentation https://www.11ty.dev/docs/config/
-
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
 	eleventyConfig.addPassthroughCopy("bundle.css");
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+    // which file extensions to process
+    extensions: 'html',
+    // optional, output image formats
+    formats: ['jpg', 'webp'],
+    // optional, output image widths
+    widths: ['auto', 400, 800],
+    // optional, attributes assigned on <img> override these values.
+    defaultAttributes: {
+        loading: 'lazy',
+        sizes: '100vw',
+        decoding: 'async',
+    },
+});
 	return {
 		dir: {
 			output: "docs",
